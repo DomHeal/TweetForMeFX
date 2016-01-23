55cf6912d91dd67671ddfb655e346ad222b6ebbf
Dominic <Dominic.heal@europe.com>
2016-01-23 18:39:11 +0000

    Initial commit

---
+++ src/main/java/Initial/Main.java
@@ -0,0 +1,33 @@
+package Initial;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("main.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        controller.init(primaryStage);
+        primaryStage.setTitle("TweetForMe - Powered by MyTweetHub");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+        primaryStage.setResizable(false);
+        primaryStage.setOnCloseRequest(e -> Platform.exit());
+
+
+//        Model model = new Model();
+//        model.connect();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/Initial/Model.java
@@ -0,0 +1,61 @@
+package Initial;
+
+import javafx.stage.FileChooser;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.awt.*;
+import java.io.*;
+import java.net.URL;
+
+/**
+ * Created by Dominic on 21-Jan-16.
+ */
+public class Model {
+    public static Twitter twitter;
+    public void connect(){
+        final ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("rloRoElkRb5suH6Kck7PjgqCo")
+                .setOAuthConsumerSecret("lZbvN35xEyioaxCT3fRnxSRmrzMcnjRAGxkVz40NzFbgRIaHsY");
+
+        try {
+            final TwitterFactory tf = new TwitterFactory(cb.build());
+            twitter = tf.getInstance();
+
+            AccessToken accessToken = null;
+            RequestToken requestToken = twitter.getOAuthRequestToken();
+                /* Opens up the URL in default browser set be user */
+            try {
+                Desktop.getDesktop().browse(new URL(requestToken.getAuthorizationURL()).toURI());
+            } catch (final Exception e) {
+                //TO-DO
+            }
+        } catch (TwitterException te) {
+            te.printStackTrace();
+        }
+    }
+    public void postTweet(String msg){
+        Status status = null;
+        try {
+            status = twitter.updateStatus(msg);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Successfully updated the status to [" + status.getText() + "].");
+    }
+
+    public void readFile() throws FileNotFoundException {
+        FileReader fileReader = new FileReader("");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+
+    }
+
+
+}
